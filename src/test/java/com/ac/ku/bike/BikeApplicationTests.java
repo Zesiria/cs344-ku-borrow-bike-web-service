f0f3be98aa4ed874f7b8ed23e8c65a5c5c47bca8
Zesiria <78583534+Zesiria@users.noreply.github.com>
2023-03-23 15:15:56 +0700

    first commit

---
+++ src/test/java/com/ac/ku/bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.ku.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
